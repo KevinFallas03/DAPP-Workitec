19b8021ce2daa373fe518c70ad73778f18010761
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-06-04 17:05:48 -0600

    transaction working

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import './App.css';
-import { useState } from 'react';
-import { ethers } from 'ethers'
-import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
-
-// Update with the contract address logged out to the CLI when it was deployed 
-const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+// import { window.ethereum } from 'window.ethereum'
 
 function App() {
-  // store greeting in local state
-  const [greeting, setGreetingValue] = useState()
+  let accounts = [];
 
-  // request access to the user's MetaMask account
-  async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
+  async function sendMoney() {
+    window.ethereum
+      .request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: '0xcd3B766CCDd6AE721141F452C550Ca635964ce71',
+            to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+            value: '0x29a2241af62c0000',
+            gasPrice: '0x09184e72a000',
+            gas: '0x5208',
+          },
+        ],
+      })
+      .then((txHash) => console.log(txHash))
+      .catch((error) => console.error);
+  };
 
-  // call the smart contract, read the current greeting value
-  async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-      } catch (err) {
-        console.log("Error: ", err)
-      }
-    }    
-  }
-
-  // call the smart contract, send an update
-  async function setGreeting() {
-    if (!greeting) return
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(greeting)
-      await transaction.wait()
-      fetchGreeting()
-    }
+  async function getAccount() {
+    accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Pago</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+        <button onClick={getAccount} >Enable window.ethereum</button>
+        <button onClick={sendMoney} >Send Eth</button>
       </header>
     </div>
   );
